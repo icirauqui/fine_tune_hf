3d916b0a76a775dc18c6132a81f5cb1b52c89c4b
icirauqui <icirauqui@gmail.com>
2023-05-22 19:38:39 +0200

    fine

--- finetuner.py
+++ finetuner.py
@@ -65,8 +65,8 @@ def train(gpu = False, path_in = "", path_out = "", data_file = ""):
 
     model = LlamaForCausalLM.from_pretrained(
         BASE_MODEL,
-        load_in_8bit=True,
-        llm_int8_enable_fp32_cpu_offload=True,
+        #load_in_8bit=True,
+        #llm_int8_enable_fp32_cpu_offload=True,
         #torch_dtype=torch.float16,
         device_map=device_map,
     )
